8d1f96fede436005e1d9549643a925c3be64933d
Daniel Koch <dkoch84@gmail.com>
2023-08-17 17:45:49 -0500

    for lazy americans

--- apps/kanagusa/app.js
+++ apps/kanagusa/app.js
@@ -31,7 +31,7 @@ function getTemperature() {
   try {
     var weatherJson = storage.readJSON('weather.json');
     var weather = weatherJson.weather;
-    return "Temp: " + Math.round(weather.temp - 273.15);
+    return "Temp: " + Math.round(1.8 * (weather.temp - 273) + 32);
   } catch (ex) {
     return "";
   }
